8987eee0df77f72b77cdaeb8629da5018b590883
huyuzhang <yuzhanghu@pku.edu.cn>
2019-03-25 21:36:16 +0800

    第一次作业

---
+++ Intern1/HDFSPicture.java
@@ -0,0 +1,195 @@
+// hw3:
+// 把图片都写入到一个文件中，为此需要在本地构建一个文件索引表，（图片名、图片在文件中的偏移地址，图片大小），把图片作为大文件的记录插入，插入时需要同时维护这个文件索引表，读取图片时也是先查找这个索引表。
+// 这个实习很有实战意义，很多深度学习项目的训练集都会包含大量的图片，训练集很大，图片很多，那么这个实习也算是给出一个解决方案。
+// 报告内容：请在报告中写明你的实现逻辑，和将每张图片单独作为小文件来存储的性能比较，并附上相应的代码段和截图。
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FSDataInputStream;
+import java.io.*;
+
+
+// 第一次运行的时候取消掉18行的注释，从而建立索引文件和HDFS文件。
+// 以后运行记得重新注释掉18行.（懒得写文件存在条件判断了）。每个索引项内容包括：fileName, index, offset, length
+public class HDFSPicture {
+	public static int fileNum = 140;
+	public static void main(String[] args){
+//		all2one();
+		readHDFS(1, "/home/alice/target1.tiff");
+		readHDFS(2, "/home/alice/target2.tiff");
+	}
+
+	public static void all2one()
+	{
+		// Conduct the Index file and make all-in-one HDFS file
+		try{
+			// ************** Index File configuration **********
+			String folderName = "/home/alice/work/non_cancer_subset00";
+			String indexName = "/home/alice/work/index.log";
+			File folderFile = new File(folderName);
+			File[] files = folderFile.listFiles();
+			File indexFile = new File(indexName);
+			// if (indexFile.exists())
+			// {
+			// 	return; // We have already construct the index file
+			// }
+			FileWriter w = new FileWriter(indexFile);
+            // ************** Index File configuration **********
+			
+			// **************  HDFS configuration ****************
+			Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", "hdfs://Master:9000");
+            conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+            FileSystem fs = FileSystem.get(conf);
+            String HDFSFileName = "all2one";
+            FSDataOutputStream HDFSw = fs.create(new Path(HDFSFileName));
+			// **************  HDFS configuration ****************
+			
+			//*************** Common configuration **************
+			int index = 0;
+			int bufferSize = 1024;
+			byte[] buffer = new byte[bufferSize];
+			int offset = 0;
+			int fileLen = 0;
+			int tmpLen = 0;
+			//*************** Common configuration **************
+
+			for (File file:files)
+			{
+				String fullFileName = folderName + "/" + file.getName();
+				BufferedInputStream bin = new BufferedInputStream(new FileInputStream(fullFileName));
+				fileLen = 0;
+				tmpLen = 0;
+				// Note that for each file, we have four lines: fileName, index, offset, length
+				w.write(fullFileName + "\n");
+				w.write(Integer.toString(index) + "\n");
+				w.write(Integer.toString(offset) + "\n");
+				while ((tmpLen = bin.read(buffer, 0, bufferSize)) > 0)
+				{
+					offset += tmpLen;
+					fileLen += tmpLen;
+					HDFSw.write(buffer, 0, tmpLen);
+				}
+				w.write(Integer.toString(fileLen) + "\n");
+				index += 1;
+				bin.close();
+				System.out.println("Finish img: " + Integer.toString(index));
+			}
+			w.close();
+			HDFSw.close();
+			fs.close();
+			fileNum = index;
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			// do nothing
+		}
+	}
+    public static void readHDFS(int index, String targetName){
+    	try{
+    		String indexName = "/home/alice/work/index.log";
+			BufferedReader indexReader = new BufferedReader(new InputStreamReader(new FileInputStream(indexName)));
+            for (int i = 0; i < fileNum; ++i)
+			{
+				String name = indexReader.readLine();
+				String _index = indexReader.readLine();
+				String offset = indexReader.readLine();
+				String length = indexReader.readLine();
+				if (index == Integer.parseInt(_index))
+				{
+                    indexReader.close();
+                    xReadHDFS(Integer.parseInt(offset), targetName);
+					break;
+				}
+			}
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+    public static void readHDFS(String rawName, String targetName){
+    	try{
+    		String indexName = "/home/alice/work/index.log";
+			BufferedReader indexReader = new BufferedReader(new InputStreamReader(new FileInputStream(indexName)));
+            for (int i = 0; i < fileNum; ++i)
+			{
+				String name = indexReader.readLine();
+				String _index = indexReader.readLine();
+				String offset = indexReader.readLine();
+				String length = indexReader.readLine();
+				if (rawName == name)
+				{
+                    indexReader.close();
+                    xReadHDFS(Integer.parseInt(offset), targetName);
+					break;
+				}
+			}
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+    public static void xReadHDFS(int _offset, String targetName){
+    	try{
+    		// **************  HDFS configuration ****************
+			Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", "hdfs://Master:9000");
+            conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+            FileSystem fs = FileSystem.get(conf);
+            String HDFSFileName = "all2one";
+            FSDataInputStream HDFSin = fs.open(new Path(HDFSFileName));
+			// **************  HDFS configuration ****************
+			
+            // ************** Index File configuration **********
+			String indexName = "/home/alice/work/index.log";
+			BufferedReader indexReader = new BufferedReader(new InputStreamReader(new FileInputStream(indexName)));
+			BufferedOutputStream targetWriter = new BufferedOutputStream(new FileOutputStream(targetName));
+			
+			for (int i = 0; i < fileNum; ++i)
+			{
+				String name = indexReader.readLine();
+				String index = indexReader.readLine();
+				String offset = indexReader.readLine();
+				String length = indexReader.readLine();
+				if (_offset == Integer.parseInt(offset))
+				{
+					HDFSin.seek(Integer.parseInt(offset));
+					int targetLength = Integer.parseInt(length);
+					int bufferSize = 1024;
+					byte[] buffer = new byte[bufferSize];
+					int tmpLength = 0;
+					int cnt = 0;
+					while ((tmpLength = HDFSin.read(buffer, 0, bufferSize)) > 0)
+					{
+						cnt += tmpLength;
+						if (cnt <= targetLength)
+						{
+							targetWriter.write(buffer, 0, tmpLength);
+							if (cnt == targetLength)
+							{
+								break;
+							}
+						}
+						else
+						{
+							targetWriter.write(buffer, 0, tmpLength - (cnt - targetLength));
+							break;
+						}
+					}
+					break;
+				}
+			}
+			HDFSin.close();
+			fs.close();
+			indexReader.close();
+			targetWriter.close();
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+}
---
+++ Intern1/HDFSWriteRead.java
@@ -0,0 +1,50 @@
+// hw2:
+// 按照助教提供的 HDFS 文件的基本创建、读写操作的 java 代码，编程实现在 HDFS 中创建大批量小文件(文件内容随意)，分析小文件的 Block 是多大，体会使用hdfs存储小文件的缺点。
+// 报告内容：请在报告中详细写明你的实验步骤、技术方法、实习体会等，附上相应的代码段和截图。
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FSDataInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 通过创建1000个小文件，测试性能。分析Block指令是"hdfs fsck hw2"
+// 结果显式，每个小文件的块大小是11B，这就意味着产生了1000个块，应该不是什么好事情
+public class HDFSWriteRead {
+	public static void main(String[] args){
+        try{
+            Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", "hdfs://Master:9000");
+            conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+            FileSystem fs = FileSystem.get(conf);
+            int fileNum = 1000;
+            for (int i = 0 ; i < fileNum; ++i)
+            {
+            	String fileName = "hw2/" + Integer.toString(i);
+            	Path file = new Path(fileName);
+            	createFile(fs, file);
+            }
+            fs.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void createFile(FileSystem fs, Path file) throws IOException{
+        byte[] buff = "Hello World".getBytes();
+        FSDataOutputStream os =fs.create(file);
+        os.write(buff,0,buff.length);
+        System.out.println("Create:"+file.getName());
+        os.close();
+    }
+    public static void readFile(FileSystem fs, Path file) throws IOException{
+            FSDataInputStream in = fs.open(file);
+            BufferedReader d = new BufferedReader(new InputStreamReader(in));
+            String content = d.readLine();
+            System.out.println(content);
+            d.close();
+            in.close();
+    }
+}
